cf8464cfe8994ea1694567676510b38fab12c048
Aaron Kessler <kessler.aaron@gmail.com>
2019-06-26 09:18:02 -0400

    added second method using lag()

--- 01_clean_and_calculate.R
+++ 01_clean_and_calculate.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(janitor)
 library(readxl)
+library(naniar)
 
 #import the data file
 orig_data <- read_excel("processed_data/vaccines_AK_June25.xlsx")
@@ -30,4 +31,44 @@ data_final <- data %>%
 head(data_final)
 
 #save result to file
-write_csv(data_final, "output/data_final.csv")
+write_csv(data_final, "output/method1_data_final.csv")
+
+
+
+####  HERE WE'LL USE AN ALTERNATIVE METHOD  ####
+
+#start with the original data file
+orig_data 
+
+#remove the existing calculated fields so we can remake them 
+method2_data <- orig_data %>% 
+  select(state, kind_pop, nme_perc, year)
+
+head(method2_data)
+
+# calculate change in nme pct using dplyr's lag()
+method2_data <- method2_data %>% 
+  mutate(
+    nme_per1000 = nme_perc * 10,
+    abs_change = nme_per1000 - lag(nme_per1000),
+    relative_change_pct = round_half_up(abs_change/nme_per1000*100, 1)
+  )
+
+head(method2_data)
+
+# the above calculated for every row, which we don't want
+# we'll remove values for 2014 rows with the naniar package's replace_with_na() 
+method2_data_final <- method2_data %>% 
+  mutate(
+    abs_change = if_else(year==2018, abs_change, 9999),
+    relative_change_pct = if_else(year==2018, relative_change_pct, 9999),
+    ) %>% 
+  naniar::replace_with_na(replace = list(abs_change = 9999,
+                                 relative_change_pct = 9999
+                                 ))
+  
+
+head(method2_data_final)
+
+#save result to file
+write_csv(method2_data_final, "output/method2_data_final.csv")
